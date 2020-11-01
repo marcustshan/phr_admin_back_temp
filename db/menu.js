5b9f297b9a90a60fa8828563f2420da1d317b4e9
Marcus Han <hts0215@naver.com>
2020-11-01 21:04:26 +0900

    공지사항 관리 기능

--- db/menu.js
+++ db/menu.js
@@ -2,33 +2,36 @@
   {
     "name": "메인화면",
     "path": "/main",
-    "invisible": true
+    "showMenu": false
   },
   {
     "name": "게시판 관리",
     "path": "/notice",
     "children": [
-      { "name": "공지사항", "path": "/notice" }
-    ]
+      { "name": "공지사항", "path": "/notice", "showMenu": true }
+    ],
+    "showMenu": true
   },
   {
     "name": "메뉴1",
     "path": "/",
     "children": [
-      { "name": "메뉴1 - 1", "path": "/" },
-      { "name": "메뉴1 - 2", "path": "/" },
-      { "name": "메뉴1 - 3", "path": "/" },
-      { "name": "메뉴1 - 4", "path": "/" }
-    ]
+      { "name": "메뉴1 - 1", "path": "/", "showMenu": true },
+      { "name": "메뉴1 - 2", "path": "/", "showMenu": true },
+      { "name": "메뉴1 - 3", "path": "/", "showMenu": true },
+      { "name": "메뉴1 - 4", "path": "/", "showMenu": true }
+    ],
+    "showMenu": true
   },
   {
     "name": "메뉴2",
     "path": "/",
     "children": [
-      { "name": "메뉴2 - 1", "path": "/" },
-      { "name": "메뉴2 - 2", "path": "/" },
-      { "name": "메뉴2 - 3", "path": "/" },
-      { "name": "메뉴2 - 4", "path": "/" }
-    ]
+      { "name": "메뉴2 - 1", "path": "/", "showMenu": true },
+      { "name": "메뉴2 - 2", "path": "/", "showMenu": true },
+      { "name": "메뉴2 - 3", "path": "/", "showMenu": true },
+      { "name": "메뉴2 - 4", "path": "/", "showMenu": true }
+    ],
+    "showMenu": true
   }
 ]
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const fs = require('fs')
 
 const _ = require('lodash')
+const dayjs = require('dayjs')
 
 /* Body Parser 모듈 */
 const bodyParser = require('body-parser')
@@ -23,11 +24,8 @@ app.get('/api/menu/list', (req, res) => {
 })
 
 app.post('/api/login', (req, res) => {
-  console.log(req.body)
   const users = JSON.parse(fs.readFileSync('./db/user.js', 'utf-8'))
   const user = _.find(users, { id: req.body.id })
-  console.log('user')
-  console.log(user)
   if(user && Object.keys(user).length > 0) {
     if(user.password === req.body.password) {
       delete user.password
@@ -50,6 +48,122 @@ app.post('/api/login', (req, res) => {
     }
     res.send(response)
   }
+})
+
+app.get('/api/notice/list', (req, res) => {
+  setTimeout(() => {
+    let noticeList = JSON.parse(fs.readFileSync('./db/notice.js', 'utf-8'))
+    noticeList = _.filter(noticeList, notice => {
+      return !notice.deleteYn || notice.deleteYn === 'N'
+    })
+    noticeList = _.orderBy(noticeList, ['noticeSeq'], ['desc'])
+
+    if (req.query.searchTitle && req.query.searchTitle.length > 0) {
+      if (req.query.searchWriter && req.query.searchWriter.length > 0) {
+        if (req.query.searchDate && req.query.searchDate.length > 0) {
+          noticeList = _.filter(noticeList, notice => {
+            return notice.title.indexOf(req.query.searchTitle) > -1
+                && notice.searchWriter.indexOf(req.query.searchWriter) > -1
+                && notice.createdDate.substring(0, 8) === req.query.searchDate
+          })
+        }
+        noticeList = _.filter(noticeList, notice => {
+          return notice.title.indexOf(req.query.searchTitle) > -1
+              && notice.searchWriter.indexOf(req.query.searchWriter) > -1
+        })
+      }
+      noticeList = _.filter(noticeList, notice => {
+        return notice.title.indexOf(req.query.searchTitle) > -1
+      })
+    }
+    else if (req.query.searchWriter && req.query.searchWriter.length > 0) {
+      if (req.query.searchDate && req.query.searchDate.length > 0) {
+        noticeList = _.filter(noticeList, notice => {
+          return notice.searchWriter.indexOf(req.query.searchWriter) > -1
+              && notice.createdDate.substring(0, 8) === req.query.searchDate
+        })
+      }
+      noticeList = _.filter(noticeList, notice => {
+        return notice.searchWriter.indexOf(req.query.searchWriter) > -1
+      })
+    }
+    else if (req.query.searchDate && req.query.searchDate.length > 0) {
+      noticeList = _.filter(noticeList, notice => {
+        return notice.createdDate.substring(0, 8) === req.query.searchDate
+      })
+    }
+
+    const beginNum = Number(req.query.page || 1) === 1 ? 0 : (req.query.page - 1) * req.query.size
+    const endNum = beginNum + 10
+    const filteredList = _.slice(noticeList, beginNum, endNum)
+
+    res.setHeader("X-Current-Page", beginNum === 0 ? 1 : beginNum)
+    res.setHeader("X-Total-Count", noticeList.length)
+    res.setHeader("X-Data-Per-Page", req.query.size || 10)
+
+    res.send(filteredList)
+  }, 500)
+})
+
+app.get('/api/notice/:noticeSeq', (req, res) => {
+  setTimeout(() => {
+    const noticeList = JSON.parse(fs.readFileSync('./db/notice.js', 'utf-8'))
+    const foundNotice = _.find(noticeList, { noticeSeq: Number(req.params.noticeSeq) })
+
+    res.send(foundNotice)
+  }, 500)
+})
+
+app.post('/api/notice/write', (req, res) => {
+  setTimeout(() => {
+    console.log(req.body)
+    const noticeList = JSON.parse(fs.readFileSync('./db/notice.js', 'utf-8'))
+    const maxSeq = _.maxBy(noticeList, 'noticeSeq').noticeSeq + 1
+    req.body.noticeSeq = maxSeq
+    req.body.createdDate = dayjs().format('YYYYMMDDHHmmss')
+    req.body.hitCount = 0
+    noticeList.push(req.body)
+    fs.writeFileSync('./db/notice.js', JSON.stringify(noticeList), { encoding:'utf8', flag:'w' })
+
+    const result = {
+      code: '0'
+    }
+    res.send(result)
+  }, 500)
+})
+
+app.post('/api/notice/modify/:noticeSeq', (req, res) => {
+  setTimeout(() => {
+    const noticeList = JSON.parse(fs.readFileSync('./db/notice.js', 'utf-8'))
+    const foundNotice = _.find(noticeList, { noticeSeq: Number(req.params.noticeSeq) })
+
+    foundNotice.title = req.body.title
+    foundNotice.content = req.body.content
+    foundNotice.modifiedDate = dayjs().format('YYYYMMDDHHmmss')
+
+    fs.writeFileSync('./db/notice.js', JSON.stringify(noticeList), { encoding:'utf8', flag:'w' })
+
+    const result = {
+      code: '0'
+    }
+    res.send(result)
+  }, 500)
+})
+
+app.post('/api/notice/delete/:noticeSeq', (req, res) => {
+  setTimeout(() => {
+    const noticeList = JSON.parse(fs.readFileSync('./db/notice.js', 'utf-8'))
+    const foundNotice = _.find(noticeList, { noticeSeq: Number(req.params.noticeSeq) })
+
+    foundNotice.deleteYn = 'Y'
+
+    fs.writeFileSync('./db/notice.js', JSON.stringify(noticeList), { encoding:'utf8', flag:'w' })
+
+    const result = {
+      code: '0'
+    }
+    res.send(result)
+  }, 500)
 })
 
 app.listen(8090, () => {
